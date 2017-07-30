55eb0322d32c283fb2ce9e67ab00fcbb4a18b089
jonasd1v <jonas_divinagracia@dlsu.edu.ph>
2017-07-30 22:02:27 +0800

    Modified main page UI

--- FinalProject/pages/index.php
+++ FinalProject/pages/index.php
@@ -80,8 +80,8 @@ if (!isset($_SESSION['username'])) {
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header"; style = "text-align: center;" 
-                            University Data <small>University Data</small>
+                        <h1 class="page-header"; style = "text-align: center;"> 
+                            <small>University Data</small>
                         </h1>
                     </div>
                 </div>
